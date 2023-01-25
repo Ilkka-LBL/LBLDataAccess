4688cf526d354143563b1d6b5c1860efd2d17b3f
Ilkka-LBL <ilkka.sipila@lewisham.gov.uk>
2023-01-25 16:59:19 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,24 @@ Created on Wed Jan 25 10:44:34 2023
 
 from setuptools import setup, find_packages
 
+import os
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+extra_files = package_files('LBLDataAccess/lookups')
+
 setup(
     name='LBLDataAccess',
     version='0.1.0',
     author='Ilkka Sipila',
     author_email='ilkka.sipila@lewisham.gov.uk',
     packages=find_packages(include=['LBLDataAccess', 'LBLDataAccess.*']),
-    package_data={'LBLDataAccess': ['config/config.json']},
-    data_files=[('lookups', ['*/*.csv', '*/*.xlsx'])],
+    package_data={'LBLDataAccess': extra_files},
     install_requires=[
         'requests',
         'pandas',
