e5af9d6f69ad082e3359721afa6d4042da9e597f
Ilkka-LBL <ilkka.sipila@lewisham.gov.uk>
2023-01-25 15:49:01 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author='Ilkka Sipila',
     author_email='ilkka.sipila@lewisham.gov.uk',
     packages=find_packages(include=['LBLDataAccess', 'LBLDataAccess.*']),
-    package_data={'LBLDataAccess': ['config/config.json', 'lookups/*']},
+    package_data={'LBLDataAccess': ['config/config.json', 'lookups/*', 'lookups/*/*']},
     install_requires=[
         'requests',
         'pandas',
